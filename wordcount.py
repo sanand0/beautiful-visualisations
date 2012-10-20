890d84eec189003e4193cf365d56ea966f58e7b4
S Anand <root.node@gmail.com>
2012-10-20 13:27:25 +0530

    wordcount.py: removed nltk dependency

--- wordcount.py
+++ wordcount.py
@@ -7,9 +7,9 @@ import sys
 import csv
 import json
 import collections
-import nltk.corpus
 
-stopwords = set(nltk.corpus.stopwords.words('english') + ['jsfoo', 'http', 'amp'])
+stopwords = set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now',
+            'jsfoo', 'http', 'amp'])
 re_word = re.compile(r'[a-z0-9\']+')
 words = collections.Counter()
 for tweet in json.loads(open(sys.argv[1]).read()):
